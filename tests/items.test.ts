ee7f75f6ea65ca404063ced429dfc3023f68f0e3
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-15 20:12:11 -0300

    :test_tube: test: Zerando o banco antes de cada teste e fechando-o após todos os testes

--- tests/items.test.ts
+++ tests/items.test.ts
@@ -1,15 +1,20 @@
 import supertest from "supertest";
 import app from "../src/app";
+import { prisma } from "../src/database/database";
 import { __createItem } from "./factory/itemFactory";
+
+beforeEach(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE items`;
+});
 
 describe("Testa POST /items ", () => {
   it("Deve retornar 201, se cadastrado um item no formato correto", async () => {
-   const item = await __createItem();
-   const response = await supertest(app).post("/items").send(item);
+    const item = await __createItem();
+    const response = await supertest(app).post("/items").send(item);
 
-   expect(response.status).toBe(201);
+    expect(response.status).toBe(201);
   });
-  
+
   it.todo("Deve retornar 409, ao tentar cadastrar um item que exista");
 });
 
@@ -20,4 +25,8 @@ describe("Testa GET /items ", () => {
 describe("Testa GET /items/:id ", () => {
   it.todo("Deve retornar status 200 e um objeto igual a o item cadastrado");
   it.todo("Deve retornar status 404 caso não exista um item com esse id");
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
 });
